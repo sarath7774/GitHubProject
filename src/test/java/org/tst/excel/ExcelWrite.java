5eb806a526a6d6044054d147890cc33c10510538
sarath7774 <sarath19kumar24@gmail.com>
2021-07-25 12:06:53 +0530

    sysout updated

--- src/test/java/org/tst/excel/ExcelWrite.java
+++ src/test/java/org/tst/excel/ExcelWrite.java
@@ -39,6 +39,7 @@ public class ExcelWrite {
 
 	public String sendText(WebElement e, String s) {
 		e.sendKeys(s);
+		System.out.println(s);
 		return s;
 	}
 	public void ClickAction(WebElement e) {
